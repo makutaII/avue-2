22e128a767ee9cd1e9a7070ce766807a8bb91efb
makutaII <igovano2@gmail.com>
2024-01-03 21:11:03 +0300

    Updates

---
+++ js/about.f8846d9c.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkavue_2"]=self["webpackChunkavue_2"]||[]).push([[443],{833:function(n,u,a){a.r(u),a.d(u,{default:function(){return l}});var e=a(3396);const t={class:"about"},r=(0,e._)("h1",null,"This is an about page",-1),s=[r];function c(n,u){return(0,e.wg)(),(0,e.iD)("div",t,s)}var i=a(89);const o={},f=(0,i.Z)(o,[["render",c]]);var l=f}}]);
+//# sourceMappingURL=about.f8846d9c.js.map
